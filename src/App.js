a2112770836d1add337edc81c887b52eacd0ae0f
Kevin Dang <kevindouglasdang@gmail.com>
2021-12-28 14:56:37 -0500

    Completed homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import keyboard from './background.jpg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+
+    const styles = {
+        background: {
+            backgroundImage: `url(${keyboard})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            width: '100vw',
+            height: '100vh'
+        },
+        header1: {
+            padding: '400px',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: 'white'
+        }
+    }
+
+        return (
+            <div style={styles.background}>
+                <div style={styles.header1}>
+                    <h1>Hi. I'm Kevin.</h1>
+                    <h2>I’m a Computer Engineering Student.</h2>
+                    <h2>You may check out more about me and my projects below.</h2>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
