1ad00f62b4caeb69a85deaa504674eda9ccfc911
Kevin Dang <kevin.dang@sheridancollege.ca>
2024-04-01 21:35:51 -0400

    Updated Work Experience and fixed mobile bug

--- src/components/text/sirtText.js
+++ src/components/text/sirtText.js
@@ -1,12 +1,11 @@
+export const TITLE = 'Work Experience';
 export const JOB_TITLE = `Junior Programmer`;
 export const COMPANY = `Screen Industries Research and Training Centre`;
 export const DURATION = `Winter 2024`;
-export const DESCRIPTION_ONE = `Developed several cognitive tasks to capture
-the user's response time when presented with a visual stimulus for a mobile
-app using the Unity Game Engine`;
-export const DESCRIPTION_TWO = `Implemented calculations for
-the average response time and the number of correct responses for each trial
-using C# and Unity's API`;
-export const DESCRIPTION_THREE = `A Muse headband utilizes the user's brain activity 
-to interact with the cognitive tasks and measure the response time for data analysis 
-and visualization`;
+export const DESCRIPTION_ONE = `Develop a mobile app using Unity and C#
+to capture the user's response and calculate their response time
+when presented with a visual stimulus`;
+export const DESCRIPTION_TWO = `I wrote technical documentation on the app 
+detailing the code architecture and Unity components for future engineers`;
+export const DESCRIPTION_THREE = `I also worked on developing a Unity plugin 
+that helps users generate character animation using an AI-powered audio prompt`;
